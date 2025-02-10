4a0cb2f0034a82ad43adb8cce0ae995c0f9e43f6
ssu <ssu@ssu.com>
2025-02-10 18:43:36 +0800

    chore: 调整docker api版本

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const config = {
 	token: Deno.env.get("APP_TOKEN") ?? "IPFS-GATEKEEPER-TOKEN",
 	whitelist: { api: Deno.env.get("WHITE_STATUS_API"), token: Deno.env.get("WHITE_STATUS_TOKEN") },
-	docker: { host: "host.docker.internal", port: 2375, version: "v1.46" },
+	docker: { host: "host.docker.internal", port: 2375, version: "v1.24" },
 	kuboContainerName: "ipfs_container",
 };
 export default config;
